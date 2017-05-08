aaac2adf7c85fd48e6df42a5dc86a5d1a6235f40
mobislan <slavi.angelov@mobi.ch>
2017-05-08 21:45:42 +0200

    handle missing protocol from files , extend script to parse iframes

--- parseHtmlForVideo.js
+++ parseHtmlForVideo.js
@@ -1,9 +1,31 @@
-var patt = /http[^,|\s]*\.mp4/g;
+var patt = /(\/\/|http)[^,|\s|>]*\.mp4/g;
 var m;
 var videoUrls = [];
-while (m = patt.exec(document.documentElement.outerHTML)) {
-  videoUrls.push(m[0]);
+
+var iframes = document.getElementsByTagName('iframe');
+for (var i in iframes) {
+  var iframeHtml = "";
+  try {
+    iframeHtml = iframes[i].contentWindow.document.body.innerHTML;
+  } catch (e) {
+
+  } 
+  while (m = patt.exec(iframeHtml)) {
+    videoUrls.push(_checkLeadingSlashes(m[0]));
+  }
 }
+
+while (m = patt.exec(document.documentElement.outerHTML)) {
+  videoUrls.push(_checkLeadingSlashes(m[0]));
+}
+
+function _checkLeadingSlashes(url){
+  if (url.substring(0, 2) === '//') {
+    return location.protocol + url;
+  }
+  return url;
+}
+
 browser.runtime.sendMessage({
   parsedUrls: videoUrls
 });
